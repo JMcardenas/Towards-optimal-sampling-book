17f08050dcdaebe0107d37267b21171fd15b3dc0
Juan Manuel Cardenas Cardenas <53189966+JMcardenas@users.noreply.github.com>
2021-08-24 16:55:23 -0400

    Update const_theta.m

--- utils/const_theta.m
+++ utils/const_theta.m
@@ -1,18 +1,22 @@
 %-------------------------------------------------------------------------%
 %
-% Filename: opt_weights.m
-% Programmer: Juan M. Cardenas
-% Part of the chapter book "Towards optimal sampling for sparse 
+% Filename: const_theta.m
+% Authors: Juan M. Cardenas and Ben Adcock
+% Part of the chapter book "Towards optimal sampling for learning sparse
 % approximation in high dimensions", Springer, 2021.
 %
-% Description: This function computes the optimal weights from sec 7.5.3
-% 
+% Description:  computes the constant theta for measurement matrix A
+%
 % Inputs: 
 % A_err - error matrix dimensions [size(err_grid), size(Index_set)]
+%
+% Output:
+% theta - constant theta 
+%-------------------------------------------------------------------------%
 
-function[theta] = const_theta(A_err)
+function theta = const_theta(A_err)
 
 sup_psi = max((abs(A_err).^2),[],2);        
-theta  = sqrt(mean(sup_psi));              % Integration via MC
+theta   = sqrt(mean(sup_psi));              % Integration via MC
 
 end
