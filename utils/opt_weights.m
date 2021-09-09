f4cd993d7b976d264785c815f06b906e75a18c8c
Juan Manuel Cardenas Cardenas <53189966+JMcardenas@users.noreply.github.com>
2021-09-09 09:48:27 -0700

    Update opt_weights.m

--- utils/opt_weights.m
+++ utils/opt_weights.m
@@ -1,19 +1,22 @@
 %-------------------------------------------------------------------------%
+%
 % Filename: opt_weights.m
-% Programmer: Juan M. Cardenas
+% Programmer: Juan M. Cardenas and Ben Adcock
 % Part of the chapter book "Towards optimal sampling for sparse 
 % approximation in high dimensions", Springer, 2021.
-%-------------------------------------------------------------------------%
+%
 % Description: generates and saves the data for Figures 7.5 and 7.6
-%-------------------------------------------------------------------------%
+%
 % Inputs:
 % A_err - error matrix dimensions [size(err_grid), size(Index_set)]
+%
 % Output: 
 % opt_mu - optimal sampling measure
 % opt_w  - optimal weights
 % theta  - theta value 
 %-------------------------------------------------------------------------%
 
+ 
 function[opt_mu,opt_w,theta] = opt_weights(A_err)
 
 % sup over the basis 
